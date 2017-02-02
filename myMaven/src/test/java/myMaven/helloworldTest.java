9ce2222d4db008fd3116679566ad64092143b61b
asus <asus@172.16.31.56>
2017-02-02 13:51:27 +0000

    Add unit test

--- myMaven/src/test/java/myMaven/helloworldTest.java
+++ myMaven/src/test/java/myMaven/helloworldTest.java
@@ -2,13 +2,22 @@ package myMaven;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class helloworldTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
-
+	  protected int value1, value2;
+	   @Before
+	   // assigning the values
+	   public void setUp(){
+	      value1 = 3;
+	      value2 = 3;
+	   }
+	   @Test
+	   // test method to add two values
+	   public void testAdd(){
+	      double result = value1 + value2;
+	      assertTrue(result == 6);
+	   }
 }
